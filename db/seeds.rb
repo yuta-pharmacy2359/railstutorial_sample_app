681ea10bebc8612f44d4b636ee13966b16cf6685
Yuta <yuta.shogai@gmail.com>
2021-01-31 18:27:22 +0000

    Finish user edit, update, index, and destroy actions 10-4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
 
 99.times do |n|
   name = Faker::Name.name
